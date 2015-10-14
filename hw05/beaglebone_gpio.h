6e64372e1657256faf40078dee8615ed618ba062
tshrjn <7tjaint@gmail.com>
2015-10-14 23:54:26 +0530

    Questions in HW5 answered

--- hw05/beaglebone_gpio.h
+++ hw05/beaglebone_gpio.h
@@ -2,17 +2,32 @@
 #ifndef _BEAGLEBONE_GPIO_H_
 #define _BEAGLEBONE_GPIO_H_
 
-
 /****************************************************************
  * Defines
  ****************************************************************/
+
+//Start address of EMFI0 SDRAM (Q1)
+#define EMIF0_SDRAM_START_ADDR 0x80000000
+
+//Start and End addresses of the GPIO Port-0
 #define GPIO0_START_ADDR 0x44e07000
 #define GPIO0_END_ADDR   0x44e09000
 #define GPIO0_SIZE (GPIO0_END_ADDR - GPIO0_START_ADDR)
 
+//Start and End addresses of the GPIO Port-1
 #define GPIO1_START_ADDR 0x4804C000
 #define GPIO1_END_ADDR   0x4804e000
 #define GPIO1_SIZE (GPIO1_END_ADDR - GPIO1_START_ADDR)
+
+//Start and End addresses of the GPIO Port-2
+#define GPIO2_START_ADDR 0x481ac000 
+#define GPIO2_END_ADDR   0x481ae000
+#define GPIO2_SIZE (GPIO2_END_ADDR - GPIO2_START_ADDR)
+
+//Start and End addresses of the GPIO Port-3
+#define GPIO3_START_ADDR 0x481ae000
+#define GPIO3_END_ADDR   0x481b0000 
+#define GPIO3_SIZE (GPIO3_END_ADDR - GPIO3_START_ADDR)
 
 #define GPIO_OE 0x134
 #define GPIO_DATAIN 0x138
